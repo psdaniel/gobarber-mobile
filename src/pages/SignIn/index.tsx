b6cf3360ee8aa688241ea0daf2300dde66bd28da
Daniel Souza <daniel.n10@hotmail.com>
2020-09-18 19:18:00 -0300

    configuração da navegação

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+    return (
+        <Container />
+    );
+}
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+    return (
+        <Container />
+    );
+}
+
+export default SignIn;
