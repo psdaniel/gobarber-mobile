e258bb8ff1ace7c291f2b09cfdeb73b904d5910d
Daniel Souza <daniel.n10@hotmail.com>
2020-09-19 18:04:27 -0300

    conexão da API com axios

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,6 +6,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -46,6 +48,12 @@ const SignUp: React.FC = () => {
             await schema.validate(data, {
                abortEarly: false, 
             });
+
+            await api.post('/users', data)
+
+            Alert.alert('Cadastro realizado com sucesso!', 'Você já pode fazer login na aplicação.')
+
+            navigation.goBack();
 
         } catch (err) {
             if(err instanceof Yup.ValidationError) {
