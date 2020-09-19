fbe811b2be889adbc27089ef57b73131b94a9bdd
Daniel Souza <daniel.n10@hotmail.com>
2020-09-19 11:20:37 -0300

    tela de cadastro e navegação entre as telas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +18,8 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+    const navigation = useNavigation();
+
     return (
         <>
         <KeyboardAvoidingView
@@ -48,7 +51,7 @@ const SignIn: React.FC = () => {
             </KeyboardAvoidingView>
 
 
-            <CreateAccountButton onPress={() => {}}>
+            <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
                 <Icon name="log-in" size={20} color='#ff9000' />
                 <CreateAccountButtonText>
                     Criar uma conta
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { 
+    Container, 
+    Title, 
+    BackToSignIn, 
+    BackToSignInText 
+} from './styles';
 
 const SignUp: React.FC = () => {
+    const navigation = useNavigation();
+
     return (
-        <Container />
+        <>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }} 
+            behavior={'padding'}
+            enabled
+            >
+
+            <ScrollView
+                keyboardShouldPersistTaps="handled" 
+                contentContainerStyle={{ flex: 1 }}>
+                <Container>
+                    <Image source={logoImg} />
+
+                    <View>
+                        <Title>Crie sua conta</Title>
+                    </View>
+
+                    <Input name="name" icon="user" placeholder="Nome" />
+                    <Input name="email" icon="mail" placeholder="E-mail" />
+                    <Input name="password" icon="lock" placeholder="Senha"/>
+
+                    <Button onPress={() => { console.log('Deu')}}>Entrar</Button>
+                </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+
+
+            <BackToSignIn onPress={() => navigation.goBack()}>
+                <Icon name="arrow-left" size={20} color='#fff' />
+                <BackToSignInText>
+                    Voltar para login
+                </BackToSignInText>
+            </BackToSignIn>
+        </>
     );
-}
+};
 
 export default SignUp;
